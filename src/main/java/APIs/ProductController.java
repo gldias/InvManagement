f3d94f01040cd37eb8665c4d705ca5774380727a
Guilherme Dias <gldias97@gmail.com>
2017-10-30 15:59:38 -0400

    API completion, but still no error responses.

--- src/main/java/APIs/ProductController.java
+++ src/main/java/APIs/ProductController.java
@@ -2,15 +2,17 @@ package APIs;
 
 import inventory_app.domain_layer.*;
 
+import org.json.JSONObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static spark.Spark.*;
+import java.util.HashMap;
 
 @Path("request")
 public class ProductController {
 
+    //TODO: Error Responses
 
     @GET
     @Path("/products/{sku}")
@@ -20,39 +22,57 @@ public class ProductController {
         return product;
     }
 
-    @GET
-    @Path("/orderFulfill/{sku}/{quantity}/{flag}")
-    public String sendOrderFulfillment(@PathParam("sku") String sku, @PathParam("quantity") int quantity, @PathParam("flag") String flag) {
-        //TODO: validation (like do we have the product already?)
-        InventoryManager.getStaticManager().addProducts(sku, quantity);
+    @POST
+    @Path("/orderFulfill/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response sendOrderFulfillment(String jsonRequest) {
+        JSONObject obj = new JSONObject(jsonRequest);
+        String sku = obj.getString("product_sku");
+        int quantity = obj.getInt("quantity");
+        String flag = obj.getString("flag");
 
-        String name = InventoryManager.getStaticManager().getProduct(sku).getName();
-        return "<User>" + "<Name>" + sku + " " + name + " added" + "</Name>" + "</User>";
+        Product product = InventoryManager.getStaticManager().addProducts(sku, quantity);
+
+        HashMap<String, String> result = new HashMap<>();
+        result.put("productID", product.getSKU());
+        return  Response.status(200).entity(result).build();
     }
 
     @GET
     @Path("/parts/{id}/{quantity}")
     @Produces(MediaType.APPLICATION_JSON)
-    public boolean requestMaterials(@PathParam("id") String partID, @PathParam("quantity") int quantity) {
-        //TODO: EDGE CASES NOT FUNCTIONING WELL
-        boolean confirmed = false;
+    public HashMap<String, Boolean> requestMaterials(@PathParam("id") String partID, @PathParam("quantity") int quantity) {
+        HashMap<String, Boolean> result = new HashMap<>();
+        result.put("confirmed", false);
         int currentQuantity = InventoryManager.getStaticManager().getPart(partID).getQuantity();
 
-        if (currentQuantity > quantity) {
+        if (currentQuantity >= quantity) {
             InventoryManager.getStaticManager().removeParts(partID, quantity);
-            confirmed = true;
-            return confirmed;
+            result.put("confirmed", true);
+            return result;
         }
-        return confirmed;
+        return result;
     }
 
-    @GET
-    @Path("/newOrder/{prodID}/{orderID}")
+    @POST
+    @Path("/newOrder/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response newOrder(@PathParam("prodID") String sku, @PathParam("orderID") String orderID) {
-        //TODO: Create Order through OrderManager when it is complete. Also, refine parameters in Order creation
-        Order order = new Order();
-        String result = "Order saved: " + order.getId();
-        return Response.status(201).entity(result).build();
+    public Response newOrder(String jsonRequest) {
+        JSONObject obj = new JSONObject(jsonRequest);
+        String sku = obj.getString("product_sku");
+        int quantity = obj.getInt("quantity");
+        String purpose = obj.getString("purpose");
+        String destination = obj.getString("destination");
+
+        //Create HashMap of Product to Quantity
+        HashMap<Item, Integer> toOrder = new HashMap<>();
+        Product product = InventoryManager.getStaticManager().getProduct(sku);
+        toOrder.put(product, quantity);
+
+        Order order = OrderManager.getStaticManager().createOrder(toOrder, destination);
+
+        HashMap<String, String> result = new HashMap<>();
+        result.put("orderID", order.getId());
+        return Response.status(200).entity(result).build();
     }
 }
--- src/main/java/inventory_app/Application.java
+++ src/main/java/inventory_app/Application.java
@@ -1,8 +1,5 @@
 package inventory_app;
-import inventory_app.domain_layer.InventoryManager;
-import inventory_app.domain_layer.PartCategory;
-import inventory_app.domain_layer.Product;
-import inventory_app.domain_layer.ProductCategory;
+import inventory_app.domain_layer.*;
 
 import static spark.Spark.*;
 
@@ -22,8 +19,6 @@ public class Application{
         manager.createPart("TestPart2", PartCategory.SCREEN, "0002", 0.01);
         manager.createPart("TestPart3", PartCategory.ANTENNA, "0003", 0.01);
 
-        System.out.println(manager.getPart("0001").getQuantity());
-        manager.removeParts("0001", 9);
-        System.out.println(manager.getPart("0001").getQuantity());
+        Order order = new Order();
     }
 }
