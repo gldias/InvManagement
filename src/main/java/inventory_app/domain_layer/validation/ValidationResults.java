2caf89e105dbfb857f7503ef50a7cf421a3cf9ce
Eric Tiano <etiano417@gmail.com>
2017-11-26 23:38:05 -0500

    update Validation Results

--- src/main/java/inventory_app/domain_layer/validation/ValidationResults.java
+++ src/main/java/inventory_app/domain_layer/validation/ValidationResults.java
@@ -1,18 +1,23 @@
 package inventory_app.domain_layer.validation;
 
+import inventory_app.domain_layer.Item;
+
 public class ValidationResults {
 
     private final boolean success;
     private final String errorMessage;
+    private Object validatedObject;
 
     public ValidationResults(){
         success = true;
         errorMessage = "";
+        validatedObject = null;
     }
 
     public ValidationResults(String _errorMessage){
         success = false;
         errorMessage = _errorMessage;
+        validatedObject = null;
 
     }
 
@@ -22,6 +27,12 @@ public class ValidationResults {
 
     public String getErrorMessage(){
         return errorMessage;
+    }
+
+    public Object getValidatedObject(){return validatedObject;}
+
+    public void setValidatedObject(Object _validatedObject){
+        validatedObject = _validatedObject;
     }
 
     public String identifier(){
